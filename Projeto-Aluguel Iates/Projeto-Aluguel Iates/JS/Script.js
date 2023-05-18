68457fba3a574f2f350e2905ff3d14f467bc8757
Lucas Floriano da Silva <112184116+lucasflorianodev@users.noreply.github.com>
2023-05-18 16:27:43 -0300

    Upload Único

--- Projeto-Aluguel Iates/Projeto-Aluguel Iates/JS/Script.js
+++ Projeto-Aluguel Iates/Projeto-Aluguel Iates/JS/Script.js
@@ -16,6 +16,30 @@ class formSubmit {
         this.form.innerHTML = this.settings.error;
     }
 
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+
+    async sendForm() {
+        await fetch(this.url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json",
+            },
+            body: "",
+        });
+        this.displaySuccsess();
+    } catch (error) {
+        this.displayError();
+        throw new error(error);
+    }
+
     init() {
         if (this.form) this.formButton.addEventslistener("click", () => this.displaySuccsess);
         return this;
@@ -29,3 +53,8 @@ const formSubmit = new formSubmit({
     error: "<h1 class='error'> Não foi possível enviar essa mensagem.</h1>",
 });
 formSubmit.init();
+
+var obj = {nome: 'nome informado.', email: 'email informado.', mensagem: 'mensagem digitada.'}
+document.getElementById("id_do_elemento").value;
+
+InserirMensagem(obj);
